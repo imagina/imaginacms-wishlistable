c830b8d15dc87bdb2412081077c7dba733f38979
Juan Escalante <jcec007@gmail.com>
2021-03-01 16:37:17 -0500

    +Update: added branch alias into the composer.json

--- Entities/Wishlistable.php
+++ Entities/Wishlistable.php
@@ -7,9 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wishlistable extends Model
 {
-    use Translatable;
 
-    protected $table = 'wishlistable__wishlistables';
     public $translatedAttributes = [];
     protected $fillable = [];
 }
