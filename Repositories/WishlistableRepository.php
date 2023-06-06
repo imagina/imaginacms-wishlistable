63fa5f0fddc200b6ad397ceba21d3b1762e1471b
Juan Escalante <jcec007@gmail.com>
2023-06-06 18:11:08 -0500

    +Update: change getItem, updateBy and deleteBy $params optional, false by default

--- Repositories/WishlistableRepository.php
+++ Repositories/WishlistableRepository.php
@@ -8,11 +8,11 @@ interface WishlistableRepository extends BaseRepository
 {
     public function getItemsBy($params);
     
-      public function getItem($criteria, $params);
+      public function getItem($criteria, $params = false);
       
       public function create($data);
     
-      public function updateBy($criteria, $data, $params);
+      public function updateBy($criteria, $data, $params = false);
     
-      public function deleteBy($criteria, $params);
+      public function deleteBy($criteria, $params = false);
 }
