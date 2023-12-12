544b2607c0019ad7f2093f0ff01a57740e43575c
William Verde <williamverde10@gmail.com>
2023-12-12 17:50:05 -0400

    Added: params configurations indexs

--- Config/config.php
+++ Config/config.php
@@ -45,5 +45,131 @@ return [
       "icon" => "fa fa-heart",
     ]
   ],
+
+  /*
+ |--------------------------------------------------------------------------
+ | Define config to the orderBy in the index page
+ |--------------------------------------------------------------------------
+ */
+  'orderBy' => [
+    'default' => 'recently',
+    'options' => [
+      'nameaz' => [
+        'title' => 'icommerce::common.sort.name_a_z',
+        'name' => 'nameaz',
+        'order' => [
+          'field' => "name",
+          'way' => "asc",
+        ]
+      ],
+      'nameza' => [
+        'title' => 'icommerce::common.sort.name_z_a',
+        'name' => 'nameza',
+        'order' => [
+          'field' => "name",
+          'way' => "desc",
+        ]
+      ],
+      'lowerprice' => [
+        'title' => 'icommerce::common.sort.price_low_high',
+        'name' => 'lowerprice',
+        'order' => [
+          'field' => "price",
+          'way' => "asc",
+        ]
+      ],
+      'higherprice' => [
+        'title' => 'icommerce::common.sort.price_high_low',
+        'name' => 'higherprice',
+        'order' => [
+          'field' => "price",
+          'way' => "desc",
+        ]
+      ],
+      'recently' => [
+        'title' => 'icommerce::common.sort.recently',
+        'name' => 'recently',
+        'order' => [
+          'field' => "created_at",
+          'way' => "desc",
+        ]
+      ]
+    ],
+  ],
+
+   /*
+  |--------------------------------------------------------------------------
+  | Layout Products - Index
+  |--------------------------------------------------------------------------
+  */
+  'layoutIndex' => [
+    'default' => 'one',
+    'options' => [
+      'four' => [
+        'name' => 'four',
+        'class' => 'col-6 col-md-4 col-lg-3',
+        'icon' => 'fa fa-th-large',
+        'status' => true
+      ],
+      'three' => [
+        'name' => 'three',
+        'class' => 'col-6 col-md-4 col-lg-4',
+        'icon' => 'fa fa-square-o',
+        'status' => true
+      ],
+      'one' => [
+        'name' => 'one',
+        'class' => 'col-12',
+        'icon' => 'fa fa-align-justify',
+        'status' => true
+      ],
+    ]
+  ],
+
+   /*
+  |--------------------------------------------------------------------------
+  | Layout Products - Index List - Show
+  |--------------------------------------------------------------------------
+  */
+  'layoutShow' => [
+    'default' => 'four',
+    'options' => [
+        'four' => [
+            'name' => 'four',
+            'class' => 'col-6 col-md-4 col-lg-3',
+            'icon' => 'fa fa-th-large',
+            'status' => true
+        ],
+        'three' => [
+            'name' => 'three',
+            'class' => 'col-6 col-md-4 col-lg-4',
+            'icon' => 'fa fa-square-o',
+            'status' => true
+        ],
+        'one' => [
+            'name' => 'one',
+            'class' => 'col-12',
+            'icon' => 'fa fa-align-justify',
+            'status' => true
+        ],
+    ]
+  ],
+
+  /*
+  |--------------------------------------------------------------------------
+  | Pagination to the index page
+  |--------------------------------------------------------------------------
+  */
+  'pagination' => [
+    "show" => true,
+    /*
+  * Types of pagination:
+  *  normal
+  *  loadMore
+  *  infiniteScroll
+  */
+    "type" => "normal"
+  ],
+
   
 ];
