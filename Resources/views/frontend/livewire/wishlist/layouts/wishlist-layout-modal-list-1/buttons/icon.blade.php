9942aac60394f41770db7d7ca1b8406245b57e19
William Verde <williamverde10@gmail.com>
2024-03-21 10:00:08 -0400

    Added: process to layout button

---
+++ Resources/views/frontend/livewire/wishlist/layouts/wishlist-layout-modal-list-1/buttons/icon.blade.php
@@ -0,0 +1,20 @@
+<div class="text-right">
+    <div id="listCheck"
+         class="mb-2 d-inline-block"
+         data-toggle="tooltip"
+         data-placement="top"
+         title=" {{trans('wishlistable::wishlists.title.add to wishlist')}}">
+      
+      <x-isite::button
+              style="outline"
+              buttonClasses="button-small rounded-pill"
+              :withIcon="true"
+              iconClass="fa-solid fa-list-check"
+              sizeLabel="16"
+              color="dark"
+              dataTarget="#wishlistAddList{{$item->id}}"
+
+      />
+      
+    </div>
+  </div>
